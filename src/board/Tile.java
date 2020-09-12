fdcdd6f17dd01f520e3a5b1df76cf1921a2b6ffc
Negarnbkhti42 <norob35@gmail.com>
2020-09-12 19:00:14 +0430

    completing the tile class

--- src/board/Tile.java
+++ src/board/Tile.java
@@ -2,21 +2,46 @@ package board;
 
 import pieces.Piece;
 
+import java.util.HashMap;
+
 public abstract class Tile {
 
-    int[] tileCoordinate;
+    protected final int[] TILE_COORDINATE;
+
+    private static final HashMap<Integer, HashMap<Integer, EmptyTile>> EMPTY_TILE_CACHE = createAllEmptyTiles();
+
+    private static HashMap<Integer, HashMap<Integer, EmptyTile>> createAllEmptyTiles(){
+        HashMap<Integer, HashMap<Integer, EmptyTile>> columns = new HashMap<>();
+
+        for (int i=0; i<8; i++){
+            columns.put(i, new HashMap<>());
+            for (int j=0; j<8; j++){
+                columns.get(i).put(j, new EmptyTile(i,j));
+            }
+        }
+
+        return columns;
+    }
 
     private Tile(int xCoordinate, int yCoordinate){
-        this.tileCoordinate =new int[] {xCoordinate, yCoordinate};
+        this.TILE_COORDINATE =new int[] {xCoordinate, yCoordinate};
     }
 
     public abstract boolean isTileOccupied();
 
     public abstract Piece getPiece();
 
+    public static Tile createTile(final int xCoordinate,final int yCoordinate,final Piece piece){
+        if (piece == null){
+            return EMPTY_TILE_CACHE.get(xCoordinate).get(yCoordinate);
+        }
+
+        return new OccupiedTile(xCoordinate, yCoordinate, piece);
+    }
+
     public static final class EmptyTile extends Tile{
 
-        private EmptyTile(int xCoordinate, int yCoordinate) {
+        private EmptyTile(final int xCoordinate, final int yCoordinate) {
             super(xCoordinate, yCoordinate);
         }
 
@@ -33,11 +58,11 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile{
 
-        private Piece piece;
+        private final Piece PIECE_ON_TILE;
 
-        private OccupiedTile(int xCoordinate, int yCoordinate, Piece piece) {
+        private OccupiedTile(final int xCoordinate, final int yCoordinate, Piece piece) {
             super(xCoordinate, yCoordinate);
-            this.piece = piece;
+            this.PIECE_ON_TILE = piece;
         }
 
         @Override
@@ -47,7 +72,7 @@ public abstract class Tile {
 
         @Override
         public Piece getPiece() {
-            return this.piece;
+            return this.PIECE_ON_TILE;
         }
     }
 }
