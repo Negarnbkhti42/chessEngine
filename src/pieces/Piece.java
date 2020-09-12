52e888a148ebca981908c093bfbc8446cecf55a8
Negarnbkhti42 <norob35@gmail.com>
2020-09-12 20:13:08 +0430

    the piece class

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,4 +1,19 @@
 package pieces;
 
-public class Piece {
+import board.Board;
+import board.Move;
+
+import java.util.Collection;
+
+public abstract class Piece {
+
+    protected final int[] piecePosition;
+    protected Alliance pieceAlliance;
+
+    Piece(final int xPosition, final int yPosition, final Alliance pieceAlliance){
+        this.piecePosition = new int[] {xPosition, yPosition};
+        this.pieceAlliance = pieceAlliance;
+    }
+
+    public abstract Collection<Move> calculateMoves(final Board board);
 }
