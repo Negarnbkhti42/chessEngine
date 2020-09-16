a3f5682c5e2854a9eedbf62ad3ee14a7cd070490
Negarnbkhti42 <norob35@gmail.com>
2020-09-16 16:05:52 +0430

    The King

---
+++ src/pieces/King.java
@@ -0,0 +1,53 @@
+package pieces;
+
+import board.Board;
+import board.Move;
+import board.Tile;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class King extends Piece {
+
+    private final static int[][] CANDIDATE_MOVE_COORDINATE = {{1, 1}, {-1, 1}, {-1, -1}, {-1, 1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+
+    King(int rowPosition, int columnPosition, Alliance pieceAlliance) {
+        super(rowPosition, columnPosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateMoves(Board board) {
+
+        ArrayList<Move> legalMoves = new ArrayList<>();
+
+        for (int[] candidateCoordinateOffset : CANDIDATE_MOVE_COORDINATE) {
+
+            int candidateDestinationRow = this.piecePositionRow;
+            int candidateDestinationColumn = this.piecePositionColumn;
+
+            candidateDestinationRow += candidateCoordinateOffset[0];
+            candidateDestinationColumn += candidateCoordinateOffset[1];
+
+            Tile candidateDestinationTile = board.getTile(candidateDestinationRow, candidateDestinationColumn);
+
+            if (Board.coordinateIsValid(candidateDestinationRow, candidateDestinationColumn)) {
+
+                if (!candidateDestinationTile.isTileOccupied()) {
+
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationRow, candidateDestinationColumn));
+
+                } else {
+
+                    if (candidateDestinationTile.getPiece().getPieceAlliance() != this.pieceAlliance) {
+                        legalMoves.add(new Move.AttackMove(board, this, candidateDestinationRow, candidateDestinationColumn, candidateDestinationTile.getPiece()));
+                    }
+
+                    break;
+                }
+            }
+        }
+
+
+        return legalMoves;
+    }
+}
